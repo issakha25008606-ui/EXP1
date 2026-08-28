f34d9c629f6adf622890b341e7cd409a4110d1d2
issakha25008606-ui <issakha.25008606@jainuniversity.ac.in>
2026-08-28 11:09:41 +0530

    Add files via upload

---
+++ bitewise.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 12 08:40:21 2026daaaaaaawsdwasdawsdwsdawadawsdwasdadwwaddawsawsdsaddawsawsdwsdawsdawsdawsdawd
+
+@author: STUDENT-PC
+"""
+
+#Bitwise Operator
+print(6 & 3)                                                            
+print(19 | 20) 
+print(9 ^ 3) 
+print(~4) 
+print(70 << 9) 
+print(12 >> 2)
+
+
+
+#Assignments Operator
+
+a = 10
+a += 10
+a -= 32
+a *= 21
+
+#identity Operator 
+
+a = 10
+b = 20 
+c = a
+
+print(a is not b)
+print(a is c)
