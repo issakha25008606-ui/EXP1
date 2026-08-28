0ce8f2ba180da055440247badcede2222d78d97b
issakha25008606-ui <issakha.25008606@jainuniversity.ac.in>
2026-08-28 11:20:54 +0530

    Add files via upload

---
+++ tpg.py
@@ -0,0 +1,39 @@
+# Integer
+a = 10
+print("Integer:", a, type(a))
+
+# Float
+b = 10.5
+print("Float:", b, type(b))
+
+# Complex
+c = 3 + 4j
+print("Complex:", c, type(c))
+
+# String
+name = "Python"
+print("String:", name, type(name))
+
+# Boolean
+is_valid = True
+print("Boolean:", is_valid, type(is_valid))
+
+# List
+numbers = [1, 2, 3, 4]
+print("List:", numbers, type(numbers))
+
+# Tuple
+values = (10, 20, 30)
+print("Tuple:", values, type(values))
+
+# Set
+items = {1, 2, 3}
+print("Set:", items, type(items))
+
+# Dictionary
+student = {"name": "Rahul", "age": 20}
+print("Dictionary:", student, type(student))
+
+# None
+x = None
+print("None:", x, type(x))
