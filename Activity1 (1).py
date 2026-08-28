4fcb31d6c4ce1dd343c9227870b680a0ea305792
issakha25008606-ui <issakha.25008606@jainuniversity.ac.in>
2026-08-28 11:05:47 +0530

    Add files via upload

---
+++ Activity1 (1).py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug 12 08:00:23 2026
+
+@author: STUDENT-PC
+"""
+
+#Arthimetic operators
+
+a = 15
+b = 4
+
+#Addition
+print("Addition",a+b)
+
+#Subtraction 
+print("Subtraction",a-b)
+
+#Multiplication
+print("Multiplication",a*b)
+
+#Division
+print("Division",a/b)
+
+#Modulus
+print("Modulus",a%b)
+
+#Exponentiation
+print("Floor Division",a**b)
+
+#Floor Division
+print("Floor Division",a//b)
+
+
+#Relational Operators
+
+x = 13
+y = 33
+
+print(x>y)
+print(x<y)
+print(x == y)
+print(x != y)
+print(x >= y)
+print(x <= y)
+
+
+
+#Logical Operators 
+
+#AND operator
+marks_pass = True
+attendance_ok = True
+
+result1 = marks_pass and attendance_ok
+print(result1)
+
+
+#NOT operator 
+fees_paid = False
+result2 = not fees_paid
+print(result2)
+
+#OR operator 
+o = True 
+p = True
+
+result3 = o or p
+print(result3)
+
+
